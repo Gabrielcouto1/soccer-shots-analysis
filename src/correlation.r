53f29805e97098eb26eefe707331a1435d045e49
Gabrielcouto1 <gabrielcouto3.14@gmail.com>
2025-06-26 09:50:24 -0300

    fix: corrected some incorrect var names and removed unused prints

--- src/correlation.r
+++ src/correlation.r
@@ -3,27 +3,30 @@ source("./src/utils/pre_process_data.r")
 check_libraries()
 
 shots <- read_csv("./datasets/shots.csv",show_col_types = FALSE)
-str(shots) 
 
 data <- pre_process_data(shots)
 
 shots <- data$dataframe
 numeric_vars <- data$numeric_vars
 
-str(shots)
-str(numeric_vars)
+# str(shots)
+# str(numeric_vars)
 
+# Verifying how many columns contains NAs in numeric vars subset
+# na_counts <- colSums(is.na(numeric_vars))
+# na_counts[na_counts > 0]
 
-numeric_vars_for_corr <- numeric_vars_for_corr %>% 
+# Mutating is_goal to be represented by a number to plot the correlation matrix
+numeric_vars_for_corr <- numeric_vars %>% 
     mutate(is_goal = as.numeric(as.character(is_goal))) 
 
 cor_matrix <- cor(numeric_vars_for_corr)
+png(file = "./plots/correlation.png", width = 3840, height = 2160, res = 300)
 
-png(file = "./plots/correlation.png", width = 1920, height = 1080, res = 150)
-
-corrplot(cor_matrix, method = "color", type = "upper", order = "hclust",
+corrplot(cor_matrix, method = "color", order = "hclust",
          addCoef.col = "black", 
          tl.col = "black", tl.srt = 45, 
          number.cex = 0.5, 
          diag = FALSE)
+         
 dev.off()
