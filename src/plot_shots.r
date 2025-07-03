d7854619aea33945ac32358d02124be0242f5dc1
Gabrielcouto1 <gabrielcouto3.14@gmail.com>
2025-07-03 10:09:06 -0300

    refactor: reduced some variables

--- src/plot_shots.r
+++ src/plot_shots.r
@@ -5,15 +5,13 @@ source("./src/utils/plots/draw_shots.r")
 
 check_libraries()
 
-df <- read_csv("./datasets/shots.csv",show_col_types = FALSE)
+df <- pre_process_data(read_csv("./datasets/shots.csv",show_col_types = FALSE))
 
-data <- pre_process_data(df)
-
-shots <- data$dataframe
+shots <- df$dataframe
 
 match_id <- sample(shots$match_id, 1)
 
 shots_from_match <- get_shots_by_match_id(match_id=match_id, dataset=shots)
 
-plot <- draw_shots(shots_from_match, save=TRUE)
+draw_shots(shots_from_match, save=TRUE)
 
